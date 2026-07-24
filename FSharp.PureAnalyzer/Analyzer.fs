051ae97529daaf9159ff8683894ccf7a8a0944e6
Erik <10636497+e-St@users.noreply.github.com>
2026-07-24 14:59:58 +0000

    fix syntax

--- FSharp.PureAnalyzer/Analyzer.fs
+++ FSharp.PureAnalyzer/Analyzer.fs
@@ -48,12 +48,17 @@ module Analyzer =
                 match su.Symbol with
                 | :? FSharpMemberOrFunctionOrValue as v when isCallableSymbol su.Symbol ->
                     let name = Name.fullNameOfMember v
+
                     if su.IsFromDefinition then
                         defCount <- defCount + 1
-                        if defNames.Count < 12 then defNames.Add(name)
+
+                        if defNames.Count < 12 then
+                            defNames.Add(name)
                     else
                         useCount <- useCount + 1
-                        if useNames.Count < 12 then useNames.Add(name)
+
+                        if useNames.Count < 12 then
+                            useNames.Add(name)
                 | _ -> ()
 
             let implFileCount = implementationFiles projectResults |> Seq.length
@@ -62,28 +67,40 @@ module Analyzer =
             let pureSetSize = knownPure.Count
 
             let sb = StringBuilder()
-            sb.AppendFormat(\"DEBUG PureAnalyzer | file={0}\", fileName) |> ignore
-            sb.AppendFormat(\" | implFiles={0}\", implFileCount) |> ignore
-            sb.AppendFormat(\" | symbolUses={0}\", allSymbolUses.Length) |> ignore
-            sb.AppendFormat(\" | defs={0}\", defCount) |> ignore
-            sb.AppendFormat(\" | uses={0}\", useCount) |> ignore
-            sb.AppendFormat(\" | graphNodes={0}\", graphSize) |> ignore
-            sb.AppendFormat(\" | nonPure={0}\", nonPureSize) |> ignore
-            sb.AppendFormat(\" | knownPure={0}\", pureSetSize) |> ignore
-            sb.Append(\" | defNames=[\").Append(String.Join(\"; \", defNames)).Append(\"]\") |> ignore
-            sb.Append(\" | useNames=[\").Append(String.Join(\"; \", useNames)).Append(\"]\") |> ignore
-            sb.Append(\" | nonPureNames=[\").Append(String.Join(\"; \", nonPure |> Set.toArray |> Array.truncate 12)).Append(\"]\") |> ignore
+            sb.Append("DEBUG PureAnalyzer | file=").Append(fileName) |> ignore
+            sb.Append(" | implFiles=").Append(implFileCount) |> ignore
+            sb.Append(" | symbolUses=").Append(allSymbolUses.Length) |> ignore
+            sb.Append(" | defs=").Append(defCount) |> ignore
+            sb.Append(" | uses=").Append(useCount) |> ignore
+            sb.Append(" | graphNodes=").Append(graphSize) |> ignore
+            sb.Append(" | nonPure=").Append(nonPureSize) |> ignore
+            sb.Append(" | knownPure=").Append(pureSetSize) |> ignore
+
+            sb.Append(" | defNames=[").Append(String.Join("; ", defNames)).Append("]")
+            |> ignore
+
+            sb.Append(" | useNames=[").Append(String.Join("; ", useNames)).Append("]")
+            |> ignore
+
+            sb
+                .Append(" | nonPureNames=[")
+                .Append(String.Join("; ", nonPure |> Set.toArray |> Array.truncate 12))
+                .Append("]")
+            |> ignore
 
             // Emit a single summary diagnostic at the top of the file (line 1)
-            let summaryRange =
-                Range.mkRange fileName (Position.mkPos 1 1) (Position.mkPos 1 2)
+            let summaryRange = Range.mkRange fileName (Position.mkPos 1 1) (Position.mkPos 1 2)
+
             messages.Add(
-                { Type = \"Pure analyzer\"
-                  Message = sb.ToString()
-                  Code = \"PURE000\"
-                  Severity = Severity.Warning
-                  Range = summaryRange
-                  Fixes = [] })
+                {
+                    Type = "Pure analyzer"
+                    Message = sb.ToString()
+                    Code = "PURE000"
+                    Severity = Severity.Warning
+                    Range = summaryRange
+                    Fixes = []
+                }
+            )
 
             // Also flag every callable definition so we can see which ones are visible
             for symbolUse in fileSymbolUses do
@@ -96,8 +113,7 @@ module Analyzer =
                     | :? FSharpMemberOrFunctionOrValue as value ->
                         let name = Name.fullNameOfMember value
                         let inNonPure = Set.contains name nonPure
-                        let msg =
-                            sprintf \"Function \"%s\" [DEBUG def] inNonPure=%b\" name inNonPure
+                        let msg = sprintf "Function '%s' [DEBUG def] inNonPure=%b" name inNonPure
                         messages.Add(Diagnostics.impureFunction msg symbolUse.Range)
                     | _ -> ()
 
@@ -111,6 +127,7 @@ module Analyzer =
                     match symbolUse.Symbol with
                     | :? FSharpMemberOrFunctionOrValue as callee ->
                         let calleeName = Name.fullNameOfMember callee
+
                         if Set.contains calleeName nonPure then
                             messages.Add(Diagnostics.impureCall calleeName symbolUse.Range)
                     | _ -> ()
@@ -124,6 +141,7 @@ module Analyzer =
                     match symbolUse.Symbol with
                     | :? FSharpMemberOrFunctionOrValue as value ->
                         let name = Name.fullNameOfMember value
+
                         if Set.contains name nonPure then
                             messages.Add(Diagnostics.impureFunction name symbolUse.Range)
                     | _ -> ()
@@ -137,24 +155,27 @@ module Analyzer =
             return! analyzeFile fileName projectResults symbolUses
         }
 
-    [<EditorAnalyzer(\"FSharp.PureAnalyzer\")>]
+    [<EditorAnalyzer("FSharp.PureAnalyzer")>]
     let pureAnalyzerEditor (ctx: EditorContext) : Async<Message list> =
         async {
             match ctx.CheckProjectResults with
             | None ->
-                // Even when there are no project results, emit a diagnostic so we know
                 let r = Range.mkRange ctx.FileName (Position.mkPos 1 1) (Position.mkPos 1 2)
+
                 return
-                    [ { Type = \"Pure analyzer\"
-                        Message = \"DEBUG PureAnalyzer | CheckProjectResults = None\"
-                        Code = \"PURE000\"
-                        Severity = Severity.Warning
-                        Range = r
-                        Fixes = [] } ]
-            | Some projectResults ->
-                return! tryAnalyzeWithProjectResults ctx.FileName projectResults
+                    [
+                        {
+                            Type = "Pure analyzer"
+                            Message = "DEBUG PureAnalyzer | CheckProjectResults = None"
+                            Code = "PURE000"
+                            Severity = Severity.Warning
+                            Range = r
+                            Fixes = []
+                        }
+                    ]
+            | Some projectResults -> return! tryAnalyzeWithProjectResults ctx.FileName projectResults
         }
 
-    [<CliAnalyzer(\"FSharp.PureAnalyzer\")>]
+    [<CliAnalyzer("FSharp.PureAnalyzer")>]
     let pureAnalyzerCli (ctx: CliContext) : Async<Message list> =
         tryAnalyzeWithProjectResults ctx.FileName ctx.CheckProjectResults
